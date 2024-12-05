7be6471a1b84e78336c5e4c5d413a75a63f1f083
Flávio Santos <flavio.pasantos@outlook.com>
2024-12-05 20:25:10 -0300

    feat: apply modify elements

--- src/app/Types/splide-react.d.ts
+++ src/app/Types/splide-react.d.ts
@@ -1,6 +1,34 @@
-declare module "@splidejs/react-splide" {
-  import { ComponentType } from "react";
+declare module '@splidejs/react-splide' {
+  import { ComponentType, CSSProperties, ReactNode } from 'react';
 
-  export const Splide: ComponentType<any>;
-  export const SplideSlide: ComponentType<any>;
+  type Options = {
+    type?: 'slide' | 'loop' | 'fade';
+    perPage?: number;
+    perMove?: number;
+    gap?: string | number;
+    arrows?: boolean;
+    pagination?: boolean;
+    autoplay?: boolean;
+    interval?: number;
+    pauseOnHover?: boolean;
+    rewind?: boolean;
+    breakpoints?: {
+      [key: string]: Options;
+    };
+    [key: string]: string | number | boolean | undefined | Options | {
+      [key: string]: Options;
+    };
+  };
+
+  export interface SplideProps {
+    options?: Options;
+    hasTrack?: boolean;
+    tag?: string;
+    className?: string;
+    style?: CSSProperties;
+    children?: ReactNode;
+  }
+
+  export const Splide: ComponentType<SplideProps>;
+  export const SplideSlide: ComponentType<{ className?: string; style?: CSSProperties; children?: ReactNode }>;
 }
